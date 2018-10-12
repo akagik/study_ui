61baa6941e65b24074cf36f614dc78bf79b76ff3
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-10-12 12:18:26 +0900

    Sample5 実装

---
+++ Assets/StudyUI/Sample5/Scripts/Sample5.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class Sample5 : MonoBehaviour
+{
+    [SerializeField] PlayableDirector playableDirector;
+    [SerializeField] PlayableAsset fadein;
+    [SerializeField] PlayableAsset fadeout;
+
+    public bool canUIControl {
+        get {
+            return playableDirector.state != PlayState.Playing;
+        }
+    }
+
+    void Start() {
+        FadeIn();
+    }
+
+    public void FadeIn() {
+        playableDirector.Play(fadein);
+    }
+
+    public void FadeOut() {
+        if (!canUIControl) {
+            return;
+        }
+        playableDirector.Play(fadeout);
+        Invoke("FadeIn", 2f);
+    }
+
+}
